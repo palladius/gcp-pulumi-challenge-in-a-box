f5d43de95e945dd816d8bdee4800e4824ef5cf12
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-09-28 09:21:49 +0200

    cleanup and now it compiles

--- code/pulumi-challenge/index.ts
+++ code/pulumi-challenge/index.ts
@@ -2,15 +2,3 @@
 
 
 // TODO(): run scripts 02 and 03
-
-// import { Script2 } from "./02-script";
-
-// let c2 = new Script2();
-
-//import filea from './02-script';
-
-//const mainmodule = { ...filea };
-
-//export = mainmodule;
-
-//import { funcA } from './02-script';
