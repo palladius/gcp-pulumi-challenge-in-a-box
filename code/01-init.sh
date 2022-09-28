94c7b19660d94301c6b166e6e0550f30a964f49d
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-09-28 09:58:56 +0200

    better docs

--- code/01-init.sh
+++ code/01-init.sh
@@ -2,7 +2,7 @@
 
 DIRNAME="pulumi-challenge"
 
-# step 1
+# step 1 (not sure if it makes sense to keep this part now that the folder exists and is populated)
 mkdir -p "$DIRNAME" &&
     cd "$DIRNAME" &&
         pulumi new gcp-typescript
