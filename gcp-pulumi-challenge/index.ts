0195876631a35bbcb95a72f420cff8243737a561
cstanger <cstanger@google.com>
2022-10-05 16:16:10 +0000

    add step 5-7

--- gcp-pulumi-challenge/index.ts
+++ gcp-pulumi-challenge/index.ts
@@ -4,3 +4,32 @@ import { CdnWebsite } from "./cdn-website";
 
 const website = new CdnWebsite("your-startup", {});
 export const cdnUrl = website.url;
+
+// Monitoring with Checkly
+// Demonstrates Standard Package usage
+import * as checkly from "@checkly/pulumi";
+import * as fs from "fs";
+
+new checkly.Check("index-page", {
+  activated: true,
+  frequency: 10,
+  type: "BROWSER",
+  locations: ["eu-west-2"],
+  script: cdnUrl.apply((url) =>
+    fs
+      .readFileSync("checkly-embed.js")
+      .toString("utf8")
+      .replace("{{websiteUrl}}", url)
+  ),
+});
+
+
+// Introducing the Dynamic Swag Provider
+import { Swag } from "./swag-provider";
+
+const swag = new Swag("your-startup", {
+  name: "YOUR NAME",
+  email: "YOUR EMAIL",
+  address: "YOUR ADDRESS",
+  size: SIZE,
+});
